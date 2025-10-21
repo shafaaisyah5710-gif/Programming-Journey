196154f215711384eb60acbce7e6415fd1d383f4
shafaaisyah5710-gif <shafa.aisyah5710@gmail.com>
2025-10-21 09:33:14 +0700

    Add files via upload

---
+++ praktik2._1_shafa_xpplg2/praktik2._1_shafa_xpplg2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik2._1_shafa_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // bilangan bulat dan teks
+            int usia = 27;
+            string nama = "Karasuma";
+
+            Console.WriteLine("Nama : " + nama);
+            Console.WriteLine("Usia : " + usia);
+
+        }
+    }
+}
